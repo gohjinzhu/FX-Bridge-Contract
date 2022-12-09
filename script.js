213df45aded1ff595eede321ba756067113d70ac
gohjinzhu <gohjinzhu@hotmail.com>
2022-12-10 04:59:05 +0800

    Run the script every 5 seconds for 1 minute Write the data with the timestamp and block height to a csv file labelled “fx-bridge token supply” Present in tabular form

--- script.js
+++ script.js
@@ -4,8 +4,29 @@ import Web3 from 'web3';
 import fxBridgeContract from './contracts/fx_bridge.js'
 import registeredTokens from './contracts/registeredTokens.js';
 import { bignumber, multiply } from 'mathjs'
+import fs from "fs";
+import { stringify } from 'csv-stringify'
 
 const web3 = new Web3("https://cloudflare-eth.com")
+const data = []
+const filename = "fx-bridge token supply.csv";
+const writableStream = fs.createWriteStream(filename);
+const columns = [
+    "Timestamp (At request)",
+    "Block Height",
+    "PUNDIX (Ether)",
+    "USDT (Ether)",
+    "DAI (Ether)",
+    "EURS (Ether)",
+    "LINK (Ether)",
+    "C98 (Ether)",
+    "WETH (Ether)",
+    "BUSD (Ether)",
+    "FRAX (Ether)",
+    "USDC (Ether)",
+];
+const stringifier = stringify({ header: true, columns: columns });
+
 /* Check instance of web3 */
 // async function getBlockNumber() {
 //     const latestBlockNumber = await web3.eth.getBlockNumber()
@@ -32,14 +53,59 @@ const getTotalSupply = async (contract) => {
         const decimals = await contract.methods.decimals().call()
         const supplyLocked = await contract.methods.balanceOf('0x6f1D09Fed11115d65E1071CD2109eDb300D80A27').call()
         //convert to Ether
-        const data = multiply(bignumber(supplyLocked), bignumber(10 ** (-decimals))).toString()
-        return data
+        const supplyLockedInEther = multiply(bignumber(supplyLocked), bignumber(10 ** (-decimals)))
+        return supplyLockedInEther.toString()
     } catch (err) {
         console.log(err)
     }
 }
-for (const token in registeredTokens) {
-    const tokenContract = registeredTokens[token](web3)
-    const data = await getTotalSupply(tokenContract)
-    console.log(`Total supply of ${token} locked in bridge is ${data} Ether.`)
+
+/* Store in object and print it */
+// const getBatchTotalSupply = async (registeredTokens) => {
+//     const timestamp = new Date()
+//     const latestBlockNumber = await web3.eth.getBlockNumber()
+//     const res = {
+//         timestamp: timestamp,
+//         blockHeight: latestBlockNumber
+//     }
+//     for (const token in registeredTokens) {
+//         const tokenContract = registeredTokens[token](web3)
+//         const supplyLockedInEther = await getTotalSupply(tokenContract)
+//         res[token] = data
+//         // console.log(`Total supply of ${token} locked in bridge is ${data} Ether`)
+//     }
+//     return res;
+// }
+
+/* Format data in rows for .csv file */
+const getBatchTotalSupply = async (registeredTokens) => {
+    const res = [];
+    const timestamp = Date()
+    const latestBlockNumber = await web3.eth.getBlockNumber()
+    res.push(timestamp, latestBlockNumber)
+    for (const token in registeredTokens) {
+        const tokenContract = registeredTokens[token](web3)
+        const supplyLockedInEther = await getTotalSupply(tokenContract)
+        //push data in this order: PUNDIX,USDT,DAI,EURS,LINK,C98,WETH,BUSD,FRAX,USDC
+        res.push(supplyLockedInEther)
+        // console.log(`Total supply of ${token} locked in bridge is ${data} Ether`)
+    }
+    return res
 }
+
+const intervalId = setInterval(async function () {
+    const res = await getBatchTotalSupply(registeredTokens)
+    data.push(res)
+}, 5000);
+
+setTimeout(() => {
+    clearInterval(intervalId)
+    data.forEach(row => {
+        stringifier.write(row);
+    });
+    stringifier.pipe(writableStream);
+    console.log("Finished writing data");
+}, 65000);
+
+
+
